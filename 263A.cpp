ccddc9982173a6ee82e5012e6ad0d3d8b4b76632
arjunnain17 <52126388+arjunnain17@users.noreply.github.com>
2020-09-23 18:45:38 +0530

    Update 263A.cpp

--- 263A.cpp
+++ 263A.cpp
@@ -3,23 +3,23 @@ using namespace std;
 int a[5][5];
 int main(){
 	int oi,og;
-	for (int i=1;i<6;i++){
+	for (int i=1;i<6;i++){ //loop to input the matrix
 		for (int g=1;g<6;g++){
 			cin >> a[i][g];
 		}
 	}
-	for (int i=1;i<6;i++){
-		for (int g=1;g<6;g++){
+	for (int i=1;i<6;i++){ 
+		for (int g=1;g<6;g++){ 
 			int c=a[i][g];
-			if (c==1){
-				oi=i;
+			if (c==1){ //leniar serch for 1.
+				oi=i; //store rows and column of 1.
 				og=g;
 
 				
 			}
 		}
 	}
-	cout << oi << " " << og << endl;
-	cout << abs(3-oi)+abs(3-og) << endl;
+	cout << oi << " " << og << endl; //this line was just a check (remove or comment out this line if you want to submit)
+	cout << abs(3-oi)+abs(3-og) << endl; //shortest path is |3-r|+|3-c|
 
 }
